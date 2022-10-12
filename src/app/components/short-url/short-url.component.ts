890b609d220fcf34776f504ae07b85734712078d
Juan Pablo <jrosas1982@gmail.com>
2022-10-11 23:14:51 -0300

    spinner

--- src/app/components/short-url/short-url.component.ts
+++ src/app/components/short-url/short-url.component.ts
@@ -11,20 +11,46 @@ export class ShortUrlComponent implements OnInit {
   urlProcesada: boolean;
   nombreUrl: string;
   urlShort: string;
+  msjError: string;
+  loading: boolean;
+  mostraError: boolean;
   constructor(private _shortUrlService: ShortUrlService) {
     this.urlProcesada = false;
     this.urlShort = '';
     this.nombreUrl = '';
+    this.msjError = '';
+    this.loading = false;
+    this.mostraError = false;
+
+  }
+  procesarURL() {
+    if (this.nombreUrl === '') {
+      this.error("Ingrese una url");
+    }
+    this.loading = true;
+    this.urlProcesada = false;
+    setTimeout(() => { this.getShortUrl() }, 2000);
   }
 
-  procesarURL(){
-      this.urlProcesada = false;
-      this._shortUrlService.getUrlShort(this.nombreUrl).subscribe(data => {
+  getShortUrl() {
+    this._shortUrlService.getUrlShort(this.nombreUrl).subscribe(data => {
       this.urlProcesada = true;
       this.urlShort = data.link;
+      this.loading = false;
+    }, error => {
+      if (error.error.description === 'The value provided is invalid.')
+        this.error("formato de Url invalida");
     });
   }
-
+  error(valor: string) {
+    this.mostraError = true;
+    this.msjError = valor;
+    setTimeout(() => {
+      this.mostraError = false;
+      this.loading = false;
+      this.nombreUrl ='';
+    }, 2000);
+  }
   ngOnInit(): void {
   }
 
